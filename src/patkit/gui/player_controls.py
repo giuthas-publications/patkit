b4ff0b4c0281a5c8f9c6a62dce4b805c87afa91f
Pertti Palo <giuthas@gmail.com>
2026-03-25 13:17:45 -0600

    Disabling more unused features.

--- src/patkit/gui/player_controls.py
+++ src/patkit/gui/player_controls.py
@@ -15,7 +15,7 @@ class PlayerControls(QWidget):
     play = pyqtSignal()
     pause = pyqtSignal()
     stop = pyqtSignal()
-    previous = pyqtSignal()
+    rewind = pyqtSignal()
     change_volume = pyqtSignal(float)
     change_muting = pyqtSignal(bool)
     change_rate = pyqtSignal(float)
@@ -45,18 +45,18 @@ class PlayerControls(QWidget):
         self.stop_button.setToolTip("Stop")
         self.stop_button.clicked.connect(self.stop)
 
-        self.previous_button = QToolButton(self)
-        self.previous_button.setIcon(
+        self.rewind_button = QToolButton(self)
+        self.rewind_button.setIcon(
             style.standardIcon(QStyle.StandardPixmap.SP_MediaSkipBackward)
         )  # noqa: E501
-        self.previous_button.setToolTip("Rewind")
-        self.previous_button.clicked.connect(self.previous)
+        self.rewind_button.setToolTip("Rewind")
+        self.rewind_button.clicked.connect(self.rewind)
 
         self.mute_button = QToolButton(self)
         self.mute_button.setIcon(style.standardIcon(
             QStyle.StandardPixmap.SP_MediaVolume))
         self.mute_button.setToolTip("Mute")
-        self.mute_button.clicked.connect(self.mute_clicked)
+        # self.mute_button.clicked.connect(self.mute_clicked)
 
         self.volume_slider = QSlider(Qt.Orientation.Horizontal, self)
         self.volume_slider.setRange(0, 100)
@@ -72,21 +72,22 @@ class PlayerControls(QWidget):
         self.rate_box.addItem("1.0x", 1.0)
         self.rate_box.addItem("2.0x", 2.0)
         self.rate_box.setCurrentIndex(1)
-        self.rate_box.setEnabled(False)
-
         # self.rate_box.activated.connect(self.update_rate)
 
         self._doSetState(QMediaPlayer.PlaybackState.StoppedState, True)
 
         layout = QHBoxLayout(self)
         layout.setContentsMargins(0, 0, 0, 0)
-        layout.addWidget(self.previous_button)
+        layout.addWidget(self.rewind_button)
         layout.addWidget(self.play_button)
         layout.addWidget(self.pause_button)
         layout.addWidget(self.stop_button)
         layout.addWidget(self.mute_button)
         layout.addWidget(self.volume_slider)
         layout.addWidget(self.rate_box)
+
+        self.mute_button.setEnabled(False)
+        self.rate_box.setEnabled(False)
 
     def state(self):
         return self.player_state
@@ -132,16 +133,16 @@ class PlayerControls(QWidget):
             QtAudio.VolumeScale.LogarithmicVolumeScale)
         self.volume_slider.setValue(round(logarithmicVolume * 100.0))
 
-    def isMuted(self):
-        return self.player_muted
+    # def isMuted(self):
+    #     return self.player_muted
 
-    @pyqtSlot(bool)
-    def set_muted(self, muted):
-        if muted != self.player_muted:
-            self.player_muted = muted
-            sp = (QStyle.StandardPixmap.SP_MediaVolumeMuted
-                  if muted else QStyle.StandardPixmap.SP_MediaVolume)
-            self.mute_button.setIcon(self.style().standardIcon(sp))
+    # @pyqtSlot(bool)
+    # def set_muted(self, muted):
+    #     if muted != self.player_muted:
+    #         self.player_muted = muted
+    #         sp = (QStyle.StandardPixmap.SP_MediaVolumeMuted
+    #               if muted else QStyle.StandardPixmap.SP_MediaVolume)
+    #         self.mute_button.setIcon(self.style().standardIcon(sp))
 
     @pyqtSlot()
     def play_clicked(self):
@@ -151,9 +152,9 @@ class PlayerControls(QWidget):
     def pause_clicked(self):
         self.pause.emit()
 
-    @pyqtSlot()
-    def mute_clicked(self):
-        self.change_muting.emit(not self.player_muted)
+    # @pyqtSlot()
+    # def mute_clicked(self):
+    #     self.change_muting.emit(not self.player_muted)
 
     # def playback_rate(self):
     #     return self.rate_box.itemData(self.rate_box.currentIndex())
@@ -167,9 +168,9 @@ class PlayerControls(QWidget):
     #     self.rate_box.addItem(f"{rate}x", rate)
     #     self.rate_box.setCurrentIndex(self.rate_box.count() - 1)
 
-    @pyqtSlot()
-    def update_rate(self):
-        self.change_rate.emit(self.playback_rate())
+    # @pyqtSlot()
+    # def update_rate(self):
+    #     self.change_rate.emit(self.playback_rate())
 
     @pyqtSlot()
     def on_volume_slider_value_changed(self):
