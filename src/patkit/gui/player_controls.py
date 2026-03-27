64bce7e212348aecf03cc35f7da7d074a1fae12f
Pertti Palo <giuthas@gmail.com>
2026-03-27 01:09:18 -0600

    Add licenseheader.

--- src/patkit/gui/player_controls.py
+++ src/patkit/gui/player_controls.py
@@ -1,5 +1,34 @@
-# Original version: Copyright (C) 2025 The Qt Company Ltd.
-# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
+#
+# Copyright (c) 2019-2026
+# Pertti Palo, Scott Moisik, Matthew Faytak, and Motoki Saito.
+#
+# This file is part of the Phonetic Analysis ToolKIT
+# (see https://github.com/giuthas/patkit/).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# The example data packaged with this program is licensed under the
+# Creative Commons Attribution-NonCommercial-ShareAlike 4.0
+# International (CC BY-NC-SA 4.0) License. You should have received a
+# copy of the Creative Commons Attribution-NonCommercial-ShareAlike 4.0
+# International (CC BY-NC-SA 4.0) License along with the data. If not,
+# see <https://creativecommons.org/licenses/by-nc-sa/4.0/> for details.
+#
+# When using the toolkit for scientific publications, please cite the
+# articles listed in README.md. They can also be found in
+# citations.bib in BibTeX format.
+#
 
 from PyQt6.QtMultimedia import QMediaPlayer, QtAudio
 from PyQt6.QtWidgets import (
